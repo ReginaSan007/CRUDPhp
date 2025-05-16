98eda9abc48f5d0b1096ccf32cf1da918d431c3c
Regina <regis_ros@outlook.com>
2025-05-16 16:50:19 -0600

    hice archivo de actualizar, update, insertar y delete, tengo problemas al insertar algo en la tabla de alumnos

--- index.php
+++ index.php
@@ -15,7 +15,10 @@ $query = mysqli_query($conn, $sql);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container mt-5">
+    <div class="container mt-2">
+        <div class="row">
+            <h1 class="text-center">Proyecto CRUD🦩</h1>
+        </div>
         <div class="row">
             <div class="col-md-3">
                 <h3>Ingerasa los Datos</h3>
@@ -27,6 +30,9 @@ $query = mysqli_query($conn, $sql);
                 </from>
             </div>
             <div class="col-md-8">
+                <div class="row">
+                    <h3 clas="text-center">Alumnos de Sistemas </h3>
+                </div>
                 <table class="table">
                     <thead class="table-success table-striped">
                         <tr>
@@ -34,10 +40,7 @@ $query = mysqli_query($conn, $sql);
                             <th>INE</th>
                             <th>Nombre (s)</th>
                             <th>Apellido</th>
-                            <th>Editar
-                                |
-                                Borrar
-                            </th>
+                            <th>Acciones</th>
                         </tr>
 
                     </thead>
@@ -59,9 +62,9 @@ $query = mysqli_query($conn, $sql);
                                 <?php echo $row['apellidos'] ?>
                             </td>
                             <td>
-                                <a href="" class="btn btn-warging">Editar</a>
+                                <a href="actualizar.php?id=<?php echo $row['cod_estudiante']?>" class="btn btn-warging">Editar</a>
                                 |
-                                <a href="" class="btn btn-danger">Borrar</a>
+                                <a href="delete.php?id=<?php echo $row['cod_estudiante']?>" class="btn btn-danger">Borrar</a>
                             </td>
                         </tr>
                         <?php 
